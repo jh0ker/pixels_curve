0a60bb0ff1e47c179626b4175f6e8f87ce0c0ac8
Jannes Höke <jhoeke@uni-bremen.de>
2015-06-11 02:07:05 +0200

    initial commit

---
+++ Colors.py
@@ -0,0 +1,13 @@
+__author__ = 'Jannes Hoeke'
+
+import pygame
+
+# Colors
+BLACK = pygame.Color(0, 0, 0)
+RED = pygame.Color(255, 0, 0)
+GREEN = pygame.Color(0, 255, 0)
+BLUE = pygame.Color(0, 0, 255)
+YELLOW = pygame.Color(255, 255, 0)
+MAGENTA = pygame.Color(255, 0, 255)
+CYAN = pygame.Color(0, 255, 255)
+WHITE = pygame.Color(255, 255, 255)
